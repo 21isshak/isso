2e7886896eec7e738762d46757ed66a2f2d5cdae
Isshak Karaagac <iskaraag@c3r6s1.42warsaw.pl>
2024-03-25 00:10:38 +0100

    hopefully last

--- ft_split.c
+++ ft_split.c
@@ -12,6 +12,104 @@
 
 #include "libft.h"
 
+static char	**free_array(char **ptr, int i)
+{
+	while (i > 0)
+	{
+		i--;
+		free(ptr[i]);
+	}
+	free(ptr);
+	return (0);
+}
+
+static int	count_words(char const *string, char c)
+{
+	int	count;
+	int	inword;
+
+	count = 0;
+	inword = 0;
+	while (*string != '\0')
+	{
+		if (*string == c)
+			inword = 0;
+		else if (inword == 0)
+		{
+			inword = 1;
+			count++;
+		}
+		string++;
+	}
+	return (count);
+}
+
+static char	*putword(char *word, char const *s, int i, int word_len)
+{
+	while (word_len > 0)
+	{
+		*word = s[i - word_len];
+		word++;
+		word_len--;
+	}
+	*word = '\0';
+	return (word);
+}
+
+static char	**ft_split_words(char const *s, char c, char **s2, int num_words)
+{
+	int	i;
+	int	word;
+	int	word_len;
+
+	i = 0;
+	word = 0;
+	word_len = 0;
+	while (word < num_words)
+	{
+		while (s[i] != '\0' && s[i] == c)
+			i++;
+		while (s[i] != '\0' && s[i] != c)
+		{
+			i++;
+			word_len++;
+		}
+		s2[word] = (char *)malloc(sizeof(char) * (word_len + 1));
+		if (s2 == NULL)
+			return (free_array(s2, word));
+		putword(s2[word], s, i, word_len);
+		word_len = 0;
+		word++;
+	}
+	s2[word] = 0;
+	return (s2);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char			**s2;
+	unsigned int	num_words;
+
+	if (s == NULL)
+		return (0);
+	num_words = count_words(s, c);
+	s2 = (char **)malloc(sizeof(char *) * (num_words + 1));
+	if (s2 == NULL)
+		return (0);
+	s2 = ft_split_words(s, c, s2, num_words);
+	return (s2);
+}
+/*static char	**free_array(char **ptr, int i)
+{
+	while (i > 0)
+	{
+		i--;
+		free(ptr[i]);
+	}
+	free(ptr);
+	return (0);
+}
+
 static char	*f_strtok(char *str, const char *delim)
 {
 	static char	*next_token = NULL;
@@ -57,6 +155,8 @@ char	**ft_split(char const *s, char c)
 	int		nw;
 	int		i;
 
+	if (s == NULL)
+		return (0);
 	str = ft_strdup(s);
 	if (f_strtok(str, &c) == 0)
 		return (NULL);
@@ -72,20 +172,43 @@ char	**ft_split(char const *s, char c)
 	while (i != nw)
 	{
 		v[i] = ft_strdup(f_strtok(NULL, &c));
+		if (v[i] == NULL)
+		{
+			free_array(v, i);
+			return (free (str), NULL);
+		}
 		i++;
 	}
 	v[i] = NULL;
 	return (free (str), v);
-}
+}*/
 /*int main()
 {
-  char ** v = ft_split("7jdfjhdf7jhsdhfbhbv7djvbnad87advbbaer87", '7');
-
-  for (int i = 0; v[i] != NULL; ++i) {
-    puts(v[i]);
-    free(v[i]);
-  }
-
-  free(v);
-  return 0;
+    char *inp = "hey,42,meraba,ben,ishak,";
+    char delimiter = ',';
+    char **result = ft_split(inp, delimiter);
+    int i;
+    int j;
+    if (result == NULL)
+    {
+        printf("nope\n");
+        return (0);
+    }
+    printf("strnigs:\n");
+    i = 0;
+    while (result[i] != NULL)
+    {
+    	printf("%s\n", result[i]);
+    	i++;
+    }
+    j = i;
+    printf("%d\n", j);
+    i = 0;
+    while (result[i] != NULL)
+    {
+    	free (result[i]);
+    	i++;
+    }
+    free(result);
+    return 0;
 }*/
